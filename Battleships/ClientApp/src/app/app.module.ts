7c90787d832521e3af863d8bc09fd83463f820c7
Benas Janušonis <benas.janusonis@outlook.com>
2022-10-27 00:55:05 +0300

    add game tiles

--- Battleships/ClientApp/src/app/app.module.ts
+++ Battleships/ClientApp/src/app/app.module.ts
@@ -1,36 +1,37 @@
+import { CdkColumnDef } from "@angular/cdk/table";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from "@angular/material/select";
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableModule } from "@angular/material/table";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorizeInterceptor } from "./authorize.interceptor";
 import { AchievementsComponent } from './components/achievements/achievements.component';
 import { AppContainerComponent } from './components/app-container/app-container.component';
+import { CreateGameSessionComponent } from './components/create-game-session/create-game-session.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FriendsListComponent } from './components/friends-list/friends-list.component';
 import { GameLobbyComponent } from './components/game-lobby/game-lobby.component';
 import { GameSessionComponent } from './components/game-session/game-session.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { PublicGamesListComponent } from './components/public-games-list/public-games-list.component';
 import { PublicUsersListComponent } from './components/public-users-list/public-users-list.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { FriendsListComponent } from './components/friends-list/friends-list.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { RegisterComponent } from './components/register/register.component';
-import { CreateGameSessionComponent } from './components/create-game-session/create-game-session.component';
-import {MatSelectModule} from "@angular/material/select";
-import {AuthorizeInterceptor} from "./authorize.interceptor";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {MatTableModule} from "@angular/material/table";
-import {CdkColumnDef} from "@angular/cdk/table";
 
 @NgModule({
   declarations: [
@@ -65,7 +66,8 @@ import {CdkColumnDef} from "@angular/cdk/table";
     HttpClientModule,
     MatSelectModule,
     MatButtonToggleModule,
-    MatTableModule
+    MatTableModule,
+    MatGridListModule
   ],
   providers: [CdkColumnDef,{
     provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
--- Battleships/ClientApp/src/app/components/public-users-list/public-users-list.component.ts
+++ Battleships/ClientApp/src/app/components/public-users-list/public-users-list.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from "../../services/http.service";
-import { PlayerService } from '../../services/player.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Player } from '../../models/player';
-
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-public-users-list',
@@ -10,6 +8,11 @@ import { Player } from '../../models/player';
   styleUrls: ['./public-users-list.component.scss']
 })
 export class PublicUsersListComponent implements OnInit {
+
+  @Input() showHeader = true;
+  @Input() showAddPlayerButton = false;
+
+  @Output() public addUserClicked = new EventEmitter<Player>();
 
   public users: Player[] = [];
 
@@ -20,6 +23,10 @@ export class PublicUsersListComponent implements OnInit {
       console.log(res);
       this.users = res;
     })
+  }
+
+  public removeUser(userId: string) {
+    this.users = this.users.filter(user => user.userId != userId);
   }
 
 }

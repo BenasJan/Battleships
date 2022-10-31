f9803afdf664b3cd9fb61bd22f2a7b882117e7a9
Tox3 <deividasmbrzts@gmail.com>
2022-10-31 18:49:00 +0200

    notification mockup

--- Battleships/ClientApp/src/app/components/notification/notification.component.ts
+++ Battleships/ClientApp/src/app/components/notification/notification.component.ts
@@ -11,7 +11,7 @@ export class NotificationComponent implements OnInit {
 
   public notifications = [
     { icon: 'emoji_events', title: 'You\'ve earned an achievement', text: 'First Win', from:'', type:'achievement' },
-    { icon: 'person_add_alt_1', title: '', text: 'Sent you a friend request', from:'Nick G.', type:'friendRequest' },
+    { icon: 'person_add_alt_1', title: '', text: 'Sent you a friend request', from:'User1', type:'request' },
     { icon: 'newspaper', title: 'New Update!', text: 'Added 5 new cosmetic skins', from:'', type:'news' },
     { icon: 'expand_less', title: 'Rank up', text: 'You\'ve reached rank [Private]', from:'', type:'info' }
   ]

bb3ddac03293f9bc0dff9749f96ab041d21d8348
Tox3 <deividasmbrzts@gmail.com>
2022-11-02 20:04:40 +0200

    shipappearance

--- Battleships/Decorators/ShipAppearance.cs
+++ Battleships/Decorators/ShipAppearance.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 
 namespace Battleships.Decorators
 {
-    public class ShipAppearance : PlayerShip
+    public class ShipAppearance : PlayerShip, IShipAppearance
     {
         public string Label { get; set; }
         public string Skin { get; set; }
         public List<string> Info { get; set; }
-        public Ship Draw()
+        public ShipAppearance Draw()
         {
             return null;
         }

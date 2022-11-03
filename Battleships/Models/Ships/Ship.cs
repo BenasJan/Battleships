420433ac995c42fad3abdcc91672ad798ef56a6c
Vladislav Ryžovas <vladislav.ryzovas@gmail.com>
2022-11-03 22:27:47 +0300

    Bridge pattern created successefully

--- Battleships/Models/Ships/Ship.cs
+++ Battleships/Models/Ships/Ship.cs
@@ -1,17 +1,40 @@
 ﻿using System.Collections.Generic;
+using Battleships.Bridge;
 
 namespace Battleships.Models.Ships
 {
-    public class Ship : BaseModel
+    public abstract class Ship : BaseModel
     {
         public string ShipTypeDiscriminator { get; set; }
 
         public string Name { get; set; }
         public string Description { get; set; }
-
+        
         public string ShipType { get; set; }
 
         public List<UserCosmetic> UserCosmetics { get; set; }
         public List<PlayerShip> PlayerShips { get; set; }
+        
+        protected IShipState _state;
+
+        public IShipState State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                _state = value;
+            }
+        }
+
+        public abstract void ChangeState();
+
+        void n()
+        {
+            State.ChangeState();
+            _state.ChangeState();
+        }
     }
 }

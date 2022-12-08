c2390024eec0f52ba5277b98368e3a0e9d8032a7
Tox3 <deividasmbrzts@gmail.com>
2022-12-08 19:07:11 +0200

    working memento example

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Battleships.Factories;
+using Battleships.Models;
+using Battleships.Models.Ships;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,11 +17,54 @@ namespace Battleships
     {
         public static void Main(string[] args)
         {
+            TestMemento();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+
+        public static void TestMemento()
+        {
+            Debug.WriteLine("Memento start");
+
+            var neutralShipFactory = new NeutralShipFactory();
+
+            PlayerShip playerShip = new PlayerShip()
+            {
+                ShipId = new Guid(),
+                Ship = neutralShipFactory.GenerateShip("Dummy"),
+                Tiles = new List<ShipTile>()
+                {
+                    new() { XCoordinate = 4, YCoordinate = 5 },
+                    new() { XCoordinate = 5, YCoordinate = 5 },
+                    new() { XCoordinate = 6, YCoordinate = 5 },
+                }
+            };
+            playerShip.PrintShipTiles();
+            playerShip.PrintShipTilesMemory();
+
+            playerShip.MoveDown();
+            Debug.WriteLine("Moved down");
+            playerShip.MoveRight();
+            Debug.WriteLine("Moved right");            
+            playerShip.MoveDown();
+            Debug.WriteLine("Moved down");
+
+            playerShip.PrintShipTiles();
+            playerShip.PrintShipTilesMemory();
+
+            playerShip.UndoMove();
+            Debug.WriteLine("Undone move");            
+            playerShip.UndoMove();
+            Debug.WriteLine("Undone move");
+
+            playerShip.PrintShipTiles();
+            playerShip.PrintShipTilesMemory();
+
+        }
     }
 }
